82f0f5c6b88f2ebdf0b43f188434cceee74411a4
NamagiriLakshmiMVL <namagirimvl@gmail.com>
2024-02-08 20:38:13 +0530

    updated

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -3,38 +3,46 @@ const messageModel = require("../models/messageModel")
 
 const router = express.Router()
 
-router.post("/sent",async(req,res)=>{
-    try{
+router.post("/sent", async (req, res) => {
+    try {
         const newMessage = new messageModel(req.body)
         await newMessage.save()
         res.send(newMessage)
     }
-    catch(err){
+    catch (err) {
         res.send(err)
     }
-   
+
 })
 
 
-router.post("/getting-msg",async(req,res)=>{
-    try{
-        const getMessage =await messageModel.find({to:req.body.items})
-        
+router.post("/getting-msg", async (req, res) => {
+    try {
+        const getMessage = await messageModel.find({ to: req.body.items })
         res.send(getMessage)
     }
-    catch(err){
+    catch (err) {
         res.send(err)
     }
-   
 })
 
 
-router.post("/getting-sent",async(req,res)=>{
-    try{
-        const send = await messageModel.find({from:req.body.data})
+router.post("/getting-sent", async (req, res) => {
+    try {
+        const send = await messageModel.find({ from: req.body.data })
         res.send(send)
     }
-    catch(err){
+    catch (err) {
+        res.send(err)
+    }
+})
+
+router.post("/deleting-msg", async (req, res) => {
+    try {
+        await messageModel.findOneAndDelete({ _id: req.body.id })
+        res.send("Deleted SuccessFully")
+    }
+    catch (err) {
         res.send(err)
     }
 })
