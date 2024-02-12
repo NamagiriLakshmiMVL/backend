6901fcf30412568a93e29fce6df1f20b4d6e796e
NamagiriLakshmiMVL <namagirimvl@gmail.com>
2024-02-12 13:21:20 +0530

    e

--- routes/signupRoute.js
+++ routes/signupRoute.js
@@ -37,7 +37,7 @@ router.post("/login", async (req, res) => {
             const storedDbPassword = newLogin.password;
             const isPasswordMatch = await bcrypt.compare(password, storedDbPassword)
             if (!isPasswordMatch) {
-                res.status(400).send({ message: "Invalid Credentials" })
+                res.send("Invalid Credentials")
                 return;
             }
 
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
 
         }
         else {
-            res.send({ message: "Invalid Credentials" })
+            res.send("Invalid Credentials")
             return
         }
     }
