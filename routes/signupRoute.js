3fe14966dae2d44f676cce6375f15b7214033499
NamagiriLakshmiMVL <namagirimvl@gmail.com>
2024-02-12 12:42:08 +0530

    hash completed

--- routes/signupRoute.js
+++ routes/signupRoute.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const userModel = require("../models/userModel")
+const genPassword = require("../helper.js")
+const bcrypt = require("bcrypt")
 const router = express.Router()
 
 router.post("/password", async (req, res) => {
     try {
-        let newUser = new userModel(req.body)
+        const { password, email, firstname, lastname, day, month, year } = req.body
+        const isUserExists = await userModel.findOne({ email: email })
+        if (isUserExists) {
+            res.send("Email Already Exists")
+            return;
+        }
+        if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#$%_&]).{8,}$/g.test(password)) {
+            res.status(400).send({ message: "Password pattern does not match" })
+            return;
+        }
+        const hashedPassword = await genPassword(password)
+        let newUser = new userModel({ password: hashedPassword, email, firstname, lastname, day, month, year })
         await newUser.save()
-        res.send("Added User Successfully")
+
+
+        res.send(newUser)
     }
     catch (err) {
         res.send(err)
@@ -14,20 +29,27 @@ router.post("/password", async (req, res) => {
 
 })
 
-router.post("/login",async(req,res)=>{
-    try{
-            const newLogin = await userModel.findOne({email:req.body.email,password:req.body.password})
-            
-            if(newLogin){
-                res.send(newLogin)
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const newLogin = await userModel.findOne({ email: req.body.email })
+        if (newLogin) {
+            const storedDbPassword = newLogin.password;
+            const isPasswordMatch = await bcrypt.compare(password, storedDbPassword)
+            if (!isPasswordMatch) {
+                res.status(400).send({ message: "Invalid Credentials in password" })
+                return;
             }
-            else
-            {
-                res.send({message:"Login Failed"})
-            }
-            
+
+            res.send({message:"Login Successfull"})
+
+        }
+        else {
+            res.send({ message: "Invalid Credentials" })
+            return
+        }
     }
-    catch(err){
+    catch (err) {
         res.send(err)
     }
 })
